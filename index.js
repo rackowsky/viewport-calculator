f57a1e34784a0fc908ac01b83703ebc3db316c66
Bart <bartek@redly.pl>
2024-01-16 02:48:09 +0100

    0.0.3 version

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function entryPoint() {
-    return "entry point"
+const breakpoint = require("./breakpoints.json")
+
+function ViewportCalculator(size, device) {
+    if (typeof size !== "number" || !Number.isInteger(size)) {
+        return console.error("The `size` value entered should be an integer.")
+    } else if (breakpoint.hasOwnProperty(device) === false) {
+        return console.error(
+            "The entered value does not exist in all data tables. Try re-entering the correct value or adding a new value in the config."
+        )
+    } else if (breakpoint.hasOwnProperty(device)) {
+        return (size / breakpoint[device]) * 100 + "vw"
+    }
 }
 
-module.exports = entryPoint
+module.exports = ViewportCalculator
